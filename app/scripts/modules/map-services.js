2e8a8a764300f93a98fd742e08bc4783d3333f38
Asad Hasan <asadpakistani@gmail.com>
2016-11-13 14:37:06 -0600

    Add android work around.

--- app/scripts/modules/map-services.js
+++ app/scripts/modules/map-services.js
@@ -119,6 +119,12 @@ function funMapService($q, $timeout, $rootScope, Faye, appSettings, services) {
                 self.getChannelToPublish(position);
                 // self.watchPositions();
                 //deferred.resolve();
+            },
+            function(error){
+                 alert(error.message);
+            }, {
+                 enableHighAccuracy: true
+                      ,timeout : 5000
             });
         } else {
             alert('Geo Location feature is not supported in this browser.');
