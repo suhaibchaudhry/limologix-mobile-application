83139f0e11bf4419571086debbf373f77b9e3a7e
Asad Hasan <asadpakistani@gmail.com>
2016-11-12 18:32:42 -0600

    Platform added

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,73 +1,81 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "id": "com.phonegap.plugins.nativesettingsopener.Settings",
+        "file": "plugins/com.phonegap.plugins.nativesettingsopener/www/settings.js",
+        "pluginId": "com.phonegap.plugins.nativesettingsopener",
+        "clobbers": [
+            "cordova.plugins.settings"
+        ]
+    },
+    {
+        "id": "cordova-plugin-fcm.FCMPlugin",
+        "file": "plugins/cordova-plugin-fcm/www/FCMPlugin.js",
+        "pluginId": "cordova-plugin-fcm",
+        "clobbers": [
+            "FCMPlugin"
+        ]
+    },
+    {
+        "id": "cordova-plugin-geolocation.geolocation",
+        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "clobbers": [
+            "navigator.geolocation"
+        ]
+    },
+    {
+        "id": "cordova-plugin-geolocation.PositionError",
+        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
+        "pluginId": "cordova-plugin-geolocation",
+        "runs": true
+    },
+    {
         "id": "cordova-plugin-network-information.network",
+        "file": "plugins/cordova-plugin-network-information/www/network.js",
+        "pluginId": "cordova-plugin-network-information",
         "clobbers": [
             "navigator.connection",
             "navigator.network.connection"
         ]
     },
     {
-        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
         "id": "cordova-plugin-network-information.Connection",
+        "file": "plugins/cordova-plugin-network-information/www/Connection.js",
+        "pluginId": "cordova-plugin-network-information",
         "clobbers": [
             "Connection"
         ]
     },
     {
-        "file": "plugins/cordova.plugins.diagnostic.api-22/www/android/diagnostic.js",
-        "id": "cordova.plugins.diagnostic.api-22.Diagnostic",
-        "clobbers": [
-            "cordova.plugins.diagnostic"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-request-location-accuracy/www/android/RequestLocationAccuracy.js",
         "id": "cordova-plugin-request-location-accuracy.RequestLocationAccuracy",
+        "file": "plugins/cordova-plugin-request-location-accuracy/www/android/RequestLocationAccuracy.js",
+        "pluginId": "cordova-plugin-request-location-accuracy",
         "clobbers": [
             "cordova.plugins.locationAccuracy"
         ]
     },
     {
-        "file": "plugins/com.phonegap.plugins.nativesettingsopener/www/settings.js",
-        "id": "com.phonegap.plugins.nativesettingsopener.Settings",
+        "id": "cordova.plugins.diagnostic.api-22.Diagnostic",
+        "file": "plugins/cordova.plugins.diagnostic.api-22/www/android/diagnostic.js",
+        "pluginId": "cordova.plugins.diagnostic.api-22",
         "clobbers": [
-            "cordova.plugins.settings"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
-        "id": "cordova-plugin-geolocation.geolocation",
-        "clobbers": [
-            "navigator.geolocation"
-        ]
-    },
-    {
-        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
-        "id": "cordova-plugin-geolocation.PositionError",
-        "runs": true
-    },
-    {
-        "file": "plugins/cordova-plugin-fcm/www/FCMPlugin.js",
-        "id": "cordova-plugin-fcm.FCMPlugin",
-        "clobbers": [
-            "FCMPlugin"
+            "cordova.plugins.diagnostic"
         ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-network-information": "1.3.0",
-    "cordova.plugins.diagnostic.api-22": "2.3.10-api-22",
-    "cordova-plugin-request-location-accuracy": "2.2.0",
     "com.phonegap.plugins.nativesettingsopener": "1.2",
+    "cordova-plugin-app-name": "1.0.0",
     "cordova-plugin-compat": "1.1.0",
-    "cordova-plugin-geolocation": "2.4.0",
     "cordova-plugin-fcm": "1.1.5",
+    "cordova-plugin-geolocation": "2.4.0",
+    "cordova-plugin-network-information": "1.3.0",
+    "cordova-plugin-request-location-accuracy": "2.2.0",
     "cordova-plugin-whitelist": "1.3.0",
-    "cordova-plugin-app-name": "1.0.0"
+    "cordova.plugins.diagnostic.api-22": "2.3.10-api-22"
 };
 // BOTTOM OF METADATA
 });
