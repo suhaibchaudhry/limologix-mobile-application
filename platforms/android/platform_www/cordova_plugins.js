bc0358e72a6ee3afa6e7d78bc928fff0212d30fd
Asad Hasan <asadpakistani@gmail.com>
2016-11-13 23:34:40 -0600

    Remove geo

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -17,20 +17,6 @@ module.exports = [
         ]
     },
     {
-        "id": "cordova-plugin-geolocation.geolocation",
-        "file": "plugins/cordova-plugin-geolocation/www/android/geolocation.js",
-        "pluginId": "cordova-plugin-geolocation",
-        "clobbers": [
-            "navigator.geolocation"
-        ]
-    },
-    {
-        "id": "cordova-plugin-geolocation.PositionError",
-        "file": "plugins/cordova-plugin-geolocation/www/PositionError.js",
-        "pluginId": "cordova-plugin-geolocation",
-        "runs": true
-    },
-    {
         "id": "cordova-plugin-network-information.network",
         "file": "plugins/cordova-plugin-network-information/www/network.js",
         "pluginId": "cordova-plugin-network-information",
@@ -69,9 +55,7 @@ module.exports.metadata =
 {
     "com.phonegap.plugins.nativesettingsopener": "1.2",
     "cordova-plugin-app-name": "1.0.0",
-    "cordova-plugin-compat": "1.1.0",
     "cordova-plugin-fcm": "1.1.5",
-    "cordova-plugin-geolocation": "2.4.0",
     "cordova-plugin-network-information": "1.3.0",
     "cordova-plugin-request-location-accuracy": "2.2.0",
     "cordova-plugin-whitelist": "1.3.0",
